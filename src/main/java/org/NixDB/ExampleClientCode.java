7d0301b0ede698f26c5031f180eac28d0fe408a2
soda4fries <39405916+soda4fries@users.noreply.github.com>
2024-01-17 18:03:48 +0800

    test something

---
+++ src/main/java/org/NixDB/ExampleClientCode.java
@@ -0,0 +1,36 @@
+package org.NixDB;
+
+import org.NixDB.Client.NixDBConnection;
+
+import java.util.Scanner;
+
+public class ExampleClientCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zoo Keeper Ip address: ");
+        String input = scanner.nextLine();
+        System.out.println("Zoo Keeper Port: ");
+        int port = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Table Name: ");
+        String tableName = scanner.nextLine();
+        NixDBConnection<String, String> connection = new NixDBConnection<String, String>(tableName, input, port, String.class, String.class);
+        while (true) {
+            System.out.println("Enter a command:");
+            String command = scanner.nextLine();
+            String[] commandParts = command.split(" ");
+            if (commandParts[0].equals("put")) {
+                connection.put(commandParts[1], commandParts[2]);
+            } else if (commandParts[0].equals("get")) {
+                System.out.println(connection.get(commandParts[1]));
+            } else if (commandParts[0].equals("remove")) {
+                connection.remove(commandParts[1]);
+            } else if (commandParts[0].equals("exit")) {
+                System.exit(0);
+            }
+        }
+
+    }
+
+}
